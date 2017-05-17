1aa66f74a9385ab9a46e5322e16b6ca5e5e694d4
BeyondCloud <a1989796@yahoo.com.tw>
2017-05-17 20:36:44 +0800

    add some op table

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <map>
+#include  <iomanip>
 using namespace std;
 
 /*
@@ -23,16 +24,41 @@ string get_opcode(char* code)
 */
 map<string, string> op_tbl;
 map<string, string>::iterator iter;
+//eax 000 , ebx 011 ,ecx 001 ,edx  010
 void tbl_init()
 {
-    op_tbl["add"] = "01C3";
-    op_tbl["sub"] = "29C3";
-    op_tbl["mul"] = "F7E0";
-    op_tbl["div"] = "F7F0";
-    op_tbl["movl"]= "C7050000";
-    op_tbl["cmp"] = "39C3";
+    op_tbl["add"] = "000111";//01C3  add %eax, %ebx
+    op_tbl["sub"] = "001010";
+    op_tbl["mul"] = "1111011111100"; // F7E0  mul %eax
+    op_tbl["div"] = "1111011111110";
+
+    op_tbl["mov"]= "10111"; //B9030000 mov $3,%ecx
+    op_tbl["cmp"]= "0011100111";//39C3 cmp %eax , %ebx
+
+
+    op_tbl["jmp"]= "";//
+    //note: jxx will change opcode if jump too far
+    op_tbl["ja"]= "011101110";//
+    op_tbl["jae"]="011100110";//
+    op_tbl["jb"]= "011100100";//jb jc same op code?
+    op_tbl["jbe"]="011101100";//
+    op_tbl["jc"]= "011100100";//
+    op_tbl["jcxz"]= "";//
+
+    op_tbl["je"]= "0111";//
+    op_tbl["jg"]= "0111";//
+    op_tbl["jge"]="0111";//
+    op_tbl["jl"]= "0111";//
+    op_tbl["jle"]="0111";//
+
+
+
+    op_tbl["call"]= "";//
+    op_tbl["rt"]= "";//
+
+
 }
-string get_tbl_value(map<string, string> M ,string str)
+string get_tbl_code(map<string, string> M ,string str)
 {
     iter = M.find(str);
     if(iter != M.end())
@@ -53,18 +79,25 @@ int main () {
       return 0;
     }
     string line;
+    static int line_counter = 0;
     while( getline (infile,line) )
     {
       char str[64];
       strcpy(str, line.c_str());
-      pch = strtok (str," \t");
+      pch = strtok (str," \t,");
+
       while (pch != NULL)
       {
-        outfile<<get_tbl_value(op_tbl,pch)<<"\n";
-        pch = strtok (NULL, " \t");
+        outfile<< setfill('0') << setw(4)<<hex<<line_counter<<"\t";
+        outfile<<get_tbl_code(op_tbl,pch)<<"\n";
+        pch = strtok (NULL, " \t,$");
+        line_counter+=2;
       }
 
+
     }
+
+
     cout<<"Done,output file to example.txt";
     infile.close();
 
