630569d2a66a066ae67accf6f929c862401619c2
BeyondCloud <a1989796@yahoo.com.tw>
2017-05-23 13:42:28 +0800

    backup before change

--- inc/ins.h
+++ inc/ins.h
@@ -4,22 +4,55 @@
 using namespace std;
 
 #define tbl_find(tbl,str) (tbl.find(str) != tbl.end())
-#define rm_16_32(x) ((x==reg16)||(x==reg32)||(x==mem16)||(x==mem32))
-#define r_16_32(x) ((x==reg16)||(x==reg32))
 
-enum operand_type{  reg8=1,
-                    reg16=1,
-                    reg32=1,
+#define type_imm8(opr_t) (opr_t.type == imm && opr_t.bits == 8)
+#define type_imm16(opr_t) (opr_t.type == imm && opr_t.bits == 16)
+#define type_imm32(opr_t) (opr_t.type == imm && opr_t.bits == 32)
+#define type_rm16_32(opr_t) (((opr_t.type == reg)||(opr_t.type == mem))&& \
+                              ((opr_t.size == 16)||(opr_t.size == 32)))
+#define type_r16_32(opr_t) ((opr_t.type == reg)&& \
+                            ((opr_t.size == 16)||(opr_t.size == 32)))
+
+#define type_r(opr_t) (opr_t.type == reg)
+#define type_imm(opr_t) (opr_t.type == imm)
+
+#define type_rm(opr_t) ((opr_t.type == reg)||(opr_t.type == mem))
+
+#define type_regA(opr_t) ( opr_t.type == reg && opr_t.reg_val == "000")
+
+/*
+enum operand_type{  r8=1,
+                    r16,
+                    r32,
                     imm8,
                     imm16,
                     imm32,
-                    mem8,
-                    mem16,
-                    mem32,
+                    m8,
+                    m16,
+                    m32,
                     not_found};
+*/
+static struct oprand_t
+{
+    string name; //ex: eax, ebx...
+    int bits;
+    enum type{reg,mem,imm,N_A};
+    unsigned long int imm_val;
+    string reg_val;
+    void clear_opr(opr_t)
+    {
+        opr_t.name="";
+        opr_t.bits=-1;
+        opr_t.type=N_A;
+        opr_t.imm_val=0;
+        opr_t.reg_val="xxx";
+    }
+}opr1,opr2;
+
 bool is_number(const string& s);
-int get_operand_type(string str);
+void init_operand(string str,oprand_t &opr);
 string str_bin2hex(string bin);
+
 void add(char* src_chp,char* dst_chp);
 
 
--- src/ins.cpp
+++ src/ins.cpp
@@ -16,31 +16,53 @@ bool is_number(const string& s)
     return !s.empty() && it == s.end();
 }
 //return enum of operand_type ,such as reg32,imm32..
-int get_operand_type(string str)
+void init_operand_type(string str,oprand_t &opr)
 {
     operand_type optype;
+    opr.name = str;
     if(tbl_find(reg32_tbl,str))
-        optype = reg32;
+    {
+        opr.bits = 32;
+        opr.type = reg;
+        opr.reg_val = reg32_tbl[str];
+    }
     else if(tbl_find(reg16_tbl,str))
-        optype = reg16;
+    {
+        opr.bits = 16;
+        opr.type = reg;
+        opr.reg_val = reg16_tbl[str];
+    }
     else if(tbl_find(reg8_tbl,str))
-        optype = reg8;
+    {
+        opr.bits = 8;
+        opr.type = reg;
+        opr.reg_val = reg8_tbl[str];
+    }
     else if(is_number(str))
     {
         unsigned long int val = stoi(str);
         assert(val<=4294967295);
         if(val<256)
-            optype = imm8;
+        {
+            opr.bits = 8;
+            opr.type = imm8;
+        }
         else if (val <65536)
-            optype = imm16;
+        {
+            opr.bits = 16;
+            opr.type = imm16;
+        }
         else
-            optype = imm32;
-
+        {
+            opr.bits = 32;
+            opr.type = imm32;
+        }
+        opr.imm_val = val;
     }
     else
     {
         cout<<"operand type not found\n";
-        optype = not_found;
+        opr.type = N_A;
     }
     return optype;
 }
@@ -62,8 +84,6 @@ string str_bin2hex(string bin)
 
 void add(char* src_chp,char* dst_chp)
 {
-
-
     //remove prefix
     char* pch[2];
     pch[0] = strtok(src_chp,"$%");
@@ -72,16 +92,55 @@ void add(char* src_chp,char* dst_chp)
 
     //determine oprand type
     int opSrc_type,opDst_type;
-    opSrc_type = get_operand_type(str_no_prefix[0]);
-    opDst_type = get_operand_type(str_no_prefix[1]);
-    cout<<opSrc_type<<"\t";
-    cout<<opDst_type<<"\t";
+    init_operand(str_no_prefix[0],str_no_prefix[1]);
+
+    cout<<opr1.type<<"\t";
+    cout<<opr2.type<<"\t";
 
     //generate corresponded opcode
     string opstr;
-    if(opSrc_type ==reg32 && opDst_type ==reg32 )
+    bool same_size = (opr1.bits ==opr2.bits);
+    //8 opcode bits
+    //first bit determine immediate mode
+    //but if you use
+
+    if(type_imm(opr1)&& type_regA(opr2)&&same_size)
+        opstr = "1";
+    else
+        opstr = "0";
+
+    opstr+="0000";
+
+    if(same_size && type_regA(opr2) && type_imm(opr1))
+        opstr+="1";
+    else
+        opstr+="0";
+
+    //d bit
+    //mem to reg
+    if(type_rm(opr1) && type_rm(r) && same_size)
+        opstr+="1";
+    else if(type_imm8(opr1) && type_rm16_32(opr2) )
+        opstr+="1";
+    else if(opr1.type == reg && opr2.type == reg && same_size)
+        opstr+="0";
+    else if(opr1.type == reg && opr2.type == mem && same_size)
+        opstr+="0";
+
+
+    //s bit
+        //d bit
+        if(rm_all(opSrc_type) ||regA_all(str_no_prefix[0]))
+            opstr+="1";
+        else
+            opstr+="0";
+        //s bit
+
+
+
+    if(opSrc_type ==r32 && opDst_type ==r32 )
     {
-        opstr = "00000001";
+
         opstr += "11";//MOD bit,11 means R/M is register
         opstr += reg32_tbl[str_no_prefix[0]];
         opstr += reg32_tbl[str_no_prefix[1]];
@@ -89,7 +148,7 @@ void add(char* src_chp,char* dst_chp)
     //else if(opSrc_type ==imm32 && ((opDst_type==reg32)||(opDst_type==mem32)))
     else if(opSrc_type ==imm8 && rm_16_32(opDst_type))
     {
-        opstr = "10000011";//bit one set to 1 means imm32
+
         opstr += "11"; //MOD
         opstr += "000"; //op extension ,000 for add imm32
         opstr += reg32_tbl[str_no_prefix[1]];
@@ -100,7 +159,7 @@ void add(char* src_chp,char* dst_chp)
         else
             opstr+=bitset<32>(stoi(str_no_prefix[0])).to_string();
     }
-    else if(opSrc_type ==imm32 && opDst_type ==reg32 )
+    else if(opSrc_type ==imm32 && r_16_32(opDst_type))
     {
 
 
